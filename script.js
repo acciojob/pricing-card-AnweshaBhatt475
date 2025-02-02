a6cba8f26d6e1f9067cfe7b3f4b742e100c6e7d9
AnweshaBhatt475 <143898640+AnweshaBhatt475@users.noreply.github.com>
2025-02-02 23:13:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+	<!-- the entire body must be written by student -->
+    <!-- <script type="text/javascript" src="./script.js">
+</script>
+ -->
+	<div class="card">
+  <img src="https://www.shutterstock.com/image-illustration/tree-shape-heart-valentines-day-600w-124858933.jpg">
+		<h1>Pricing Card</h1>
+		<p>Price: Rs.200</p>
+		<p>Description: Lorem ipsum dolor sit amet,
+		consectetur adipiscing alit.</p>
+		<button>Buy Now</button>
+		
+	</div>
+</body>
+
+</html>
